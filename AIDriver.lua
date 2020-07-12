ed1fd309d8710363a4dcb50bd4ad5a8077aecf67
Peter Vaiko <github@vaiko.us>
2020-07-12 10:34:37 -0500

    wip

--- AIDriver.lua
+++ AIDriver.lua
@@ -233,7 +233,7 @@ function AIDriver:beforeStart()
 		-- is in aiTrafficCollisionTranslation, if you do a setTranslation() it won't remain there...
 		self.vehicle.spec_aiVehicle.aiTrafficCollisionTranslation[2] = -1000
 	end
-	self.trafficConflictDetector = TrafficConflictDetector(self.vehicle, self.course)
+	self:createTrafficConflictDetector()
 end
 
 --- Start driving
@@ -1601,6 +1601,10 @@ end
 
 function AIDriver:onUnBlocked()
 	self:debug('Unblocked...')
+end
+
+function AIDriver:createTrafficConflictDetector()
+	self.trafficConflictDetector = TrafficConflictDetector(self.vehicle, self.course)
 end
 
 function AIDriver:updateTrafficConflictDetector()
